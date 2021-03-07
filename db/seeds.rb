d37f5784f848f63b773afc619caf18e99f4387a0
Michele Dorazio <michele.dorazio96@gmail.com>
2021-03-07 18:08:54 +0100

    properly seeded ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
+require 'json'
+require 'open-uri'
+
 puts "Cleaning Database..."
 
 Ingredient.destroy_all
 
 puts "Creating Ingredients..."
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+link = open(url).read
+cocktails = JSON.parse(link)
+cocktails["drinks"].each do |cocktail|
+  Ingredient.create(name: cocktail["strIngredient1"])
+end
+
+puts "Your data is ready 🍪"
